3bc03988f9573a2621730c145247677bccd4c5b3
nyg0o0 <yaya56790@gmail.com>
2016-05-14 16:53:08 +0800

    comments

--- TestBTSort.java
+++ TestBTSort.java
@@ -1,16 +1,29 @@
-/* Import java api*/
+/**
+	Student ID: U10116054
+	Student Name: YU-HSIN, CHEN
+	This is a program to sort numbers with binary tree.
+	P.S: This program refer to the textbook. Chapter25. BST and TestBST.java
+*/
+
+
+/** Import java api*/
 import java.util.*;
 
+/** Test program */
 public class TestBTSort<E>{
-	/* Main method */
+	/** Main method */
 	public static void main(String[] args) {
-		/* Test array */
+		/** Test array */
 		Integer[] numbers = {-44, -5, -3, 3, 3, 1, -4, 0, 1, 2, 4, 5, 53};
+		
+		// Display the arry before sorted
 		System.out.print("\nBefore Sorted: ");
 		for(int i = 0; i<numbers.length ; i++){
 			System.out.print(numbers[i] + " ");
 		}
-		 BTSort<Integer> intTree = new BTSort<>(numbers);
+		
+		// Display the arry after sorted
+		BTSort<Integer> intTree = new BTSort<>(numbers);
 		System.out.print("\nAfter Sorted: ");
 		intTree.inOrder();
 	}	
